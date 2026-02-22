557ffcbd1e42f399884af149e50f7286a32bb790
Nicolas25Vlad <nicolas25vlad@gmail.com>
2026-02-22 13:00:08 -0300

    finalizando conexão de adm com o front, todas as conexões feitas

---
+++ src/main/JavaScript/code/adm/ConnectionAdm.js
@@ -0,0 +1,96 @@
+import Adm from './Adm.js';
+
+const url = 'http://localhost:8080/TrabalhoDSV2/';
+
+async function logarAdm(email=String, senha=String) {
+    try {
+
+        const login = {
+            email: email,
+            senha: senha
+        }
+
+        const resposta = await fetch(`${url}app/adm/logar`, {
+            method: 'POST',
+            headers: {
+                //seta o tipo de conteudo para json, para o backend entender que é um json
+                'Content-Type': 'application/json',
+            },
+            //transforma para string o json adm
+            body: JSON.stringify(login)
+        });
+
+        // Verificar se a resposta foi ok
+        if (!resposta.ok) {
+            throw new Error(`Erro HTTP: ${resposta.status}`);
+        }
+
+        // Converter resposta para json
+        const dadosResposta = await resposta.json();
+        
+
+        //retorna os dados da resposta para o frontend
+        const retorno = dadosResposta.retorno;
+        return retorno;
+        
+        //try e catch, igual o java
+    } catch (e) {
+        //mensagem de erro, como no front é menos comum n tem um tratamento completo
+        console.error('erro ao enviar:', e);
+    }
+    
+}
+
+
+
+
+async function exibirAdmPorEmail(email=String) {
+    try {
+        //constante do tipo de busca
+        const tipo = "email";
+        //cria a url com os parametros, e usa fetch para fazer a requisição get
+        const resposta = await fetch(`${url}app/adm/exibir?tipo=${tipo}&email=${(email)}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        //verifica se a resposta foi ok
+        if (!resposta.ok) {
+            throw new Error(`Erro HTTP: ${resposta.status}`);
+        }
+
+        //converte a resposta para json
+        const dadosResposta = await resposta.json();
+        //separa da resposta de sucesso para resposta de adm
+        const admsArray = dadosResposta.adms;
+
+
+        if (Array.isArray(admsArray)) {
+            // lista para armazenar objetos adm
+            const listaAdms = [];
+            //pega o lenght da respsota
+            const length = admsArray.length;
+            //for de respostas
+            for (let i = 0; i < length; i++) {
+
+                //converte cada json da resposta para um objeto adm usando o metodo deJson da classe adm, e adiciona na lista de adms
+                const admJson = admsArray[i];
+                //converte para objeto
+                const admObj = Adm.deJson(admJson);
+                //coloca na lista
+                listaAdms.push(admObj);
+
+            }
+            //retorna lista de retorna
+            return listaAdms;
+
+        }
+    } catch (e) {
+        console.error('Erro ao buscar adm por nome:', e);
+    }
+
+}
+
+
+export { logarAdm, exibirAdmPorEmail };
