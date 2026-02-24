6fd0b5e4d5b5beeafdf0fc499d2bed8484af6d1f
Lucas-Cayres-Porto <lucas.porto.tech@gmail.com>
2026-02-24 18:50:26 -0300

    feat(exibirAluno): criando o método para buscar um aluno pela matricula

--- src/main/java/dao/AlunosDAO.java
+++ src/main/java/dao/AlunosDAO.java
@@ -248,6 +248,27 @@ public class AlunosDAO {
             return alunos;
         }
     }
+    public List<Document> buscarPorMatricula(String matricula){
+        List<Document> alunos = new ArrayList<>();
+        Document filtro = new Document();
+        filtro.append("tipo", "aluno");      // Filtra pelo tipo
+        filtro.append("dados_aluno.matricula",matricula);
+        MongoCursor<Document> cursor = colecao.find(filtro).iterator();
+        try {
 
+
+            //insere no json
+            while (cursor.hasNext()) {
+                alunos.add(cursor.next());
+            }
+        }catch (Exception e){
+            ExceptionHandler eh = new ExceptionHandler(e);
+            eh.printExeption();
+        }
+        finally {
+            cursor.close();
+            return alunos;
+        }
+    }
 
 }
